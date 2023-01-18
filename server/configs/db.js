a07ac7454d05f50859a5e76ac4ac21fd99a8c3d7
Nikankhadka <95460437+Nikankhadka@users.noreply.github.com>
2023-01-18 18:15:21 +0545

    updatedb.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,8 +1,7 @@
 const mongoose=require("mongoose");
 
 async function connect(){
-    await mongoose.connect("mongodb+srv://nick11444:nikhildon1@passwordmanager.wafw4zn.mongodb.net/test
-").then(()=>{
+    await mongoose.connect("mongodb+srv://nick11444:nikhildon1@passwordmanager.wafw4zn.mongodb.net/test").then(()=>{
         console.log("connected to passwordmanager database")
     }).catch((err)=>{
         // gives error message wrong connection
